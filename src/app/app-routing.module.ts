fb791ee2572ea6e306069db96224f769ba7544c4
Mohammad Fareed <mohammad.fareed@joshsoftware.com>
2025-01-25 16:18:04 +0530

    Assets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationModule } from './authentication/authentication.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    redirectTo : 'auth',
+    pathMatch : 'full'
+  },
+  {
+    path : 'path',
+    loadChildren : () => import('./authentication/authentication.module').then(m =>m.AuthenticationModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
